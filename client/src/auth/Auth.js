b90268775c8a0c6a437f58308674906dc7215ff1
Pemberai Sweto <pembeweb@yahoo.com>
2020-07-05 14:35:27 +0200

    fix: update auth files

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -33,8 +33,8 @@ export default class Auth {
   handleAuthentication () {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        console.log('Access token: ', authResult.accessToken)
-        console.log('id token: ', authResult.idToken)
+        // console.log('Access token: ', authResult.accessToken)
+        // console.log('id token: ', authResult.idToken)
         this.setSession(authResult)
       } else if (err) {
         this.history.replace('/')
